464f78a2ee82f3320a40f050706982b9c1bd87dd
Tymoteusz Kwiecinski <tymoteuszkwiecinski@gmail.com>
2026-03-06 17:44:11 +0100

    what if we use conda?

--- idisc/models/ops/make.sh
+++ idisc/models/ops/make.sh
@@ -2,4 +2,12 @@
 
 export TORCH_CUDA_ARCH_LIST="5.2 6.0 6.1 7.0 7.5 8.0 8.6+PTX" 
 # export FORCE_CUDA=1 #if you do not actually have cuda, workaround
-python setup.py build install --prefix $VIRTUAL_ENV
+
+# Support both venv/virtualenv and conda/micromamba environments.
+PREFIX="${VIRTUAL_ENV:-$CONDA_PREFIX}"
+if [ -z "$PREFIX" ]; then
+  echo "Error: neither VIRTUAL_ENV nor CONDA_PREFIX is set. Activate your environment first."
+  exit 1
+fi
+
+python setup.py build install --prefix "$PREFIX"
